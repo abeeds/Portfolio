55f7a28c289c058e9e702e5158822abea4bfbc6d
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-10 14:13:49 -0500

    fetch experience from json

--- display.js
+++ display.js
@@ -2,13 +2,27 @@ const experience = "https://raw.githubusercontent.com/abeeds/Portfolio/refs/head
 const projects = "https://raw.githubusercontent.com/abeeds/Portfolio/refs/heads/main/projects.json";
 
 
+fetch(experience)
+    .then(response => response.json())
+    .then(data => {
+        const exp_div = document.querySelector('.experience');
+        const exp_arr = Object.keys(data);
+
+        exp_div.innerHTML = `
+            <ul>
+                ${exp_arr.map(key => `<li>${key}</li>`).join('')}
+            </ul>
+        `;
+    }).catch(error => console.error('Error fetching experience:', error));
+
+
 fetch(projects)
     .then(response => response.json())
     .then(data => {
-        const projects_div = document.querySelector('.projects');
+        const proj_div = document.querySelector('.projects');
         const proj_arr = Object.keys(data);
 
-        projects_div.innerHTML = `
+        proj_div.innerHTML = `
             <ul>
                 ${proj_arr.map(key => `<li>${key}</li>`).join('')}
             </ul>
